5eff7bf5582ab67f5aa036194d7529b7fde0d93f
Pengcheng Tan <markqq@yeah.net>
2018-01-09 11:24:19 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,14 @@
 <?php
-    $con = mysql_connect("127.0.0.1","root","");
+    $databaseName = "";
+    $databaseServer = "";
+    $databaseUsername = "";
+    $databasePassword = "";
+
+    $con = mysql_connect($databaseServer,$databaseUsername,$databasePassword);
     mysql_query("SET NAMES 'utf8'");
     mysql_query("SET CHARACTER SET utf8");
     if(!$con){
         die(mysql_error());
     }
-    mysql_select_db("tan90",$con);
+    mysql_select_db($databaseName,$con);
 ?>
